881fde4e2c8c60442fa8bbc9fa2dfd726d62fca5
Imane Abdennour <imane.abdennour0@gmail.com>
2021-02-18 15:47:28 +0100

    Fixing CommandCntroller : using DTO instead of object directly

--- GestionCommande/Controllers/CommandsController.cs
+++ GestionCommande/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
-﻿using GestionCommande.Data;
+﻿using AutoMapper;
+using GestionCommande.Data;
+using GestionCommande.DTOs;
 using GestionCommande.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,25 +15,31 @@ namespace GestionCommande.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDTO>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDTO>>(commandItems));
         }
 
         // Get api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult<Command> GetCommandById(int id)
+        public ActionResult<CommandReadDTO> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
-            return Ok(commandItem);
+
+            if(commandItem != null)
+                return Ok(_mapper.Map<CommandReadDTO>(commandItem));
+
+            return NotFound();
         }
     }
 }
--- GestionCommande/DTOs/CommandReadDTO.cs
+++ GestionCommande/DTOs/CommandReadDTO.cs
@@ -15,4 +15,4 @@ namespace GestionCommande.DTOs
 
     }
 }
-}
+
