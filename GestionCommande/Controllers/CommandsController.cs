5c6661f5fc09ae34398e5293b1a3b5b17ff25d57
Imane Abdennour <imane.abdennour0@gmail.com>
2021-02-18 12:27:46 +0100

    first commit

---
+++ GestionCommande/Controllers/CommandsController.cs
@@ -0,0 +1,37 @@
+﻿using GestionCommande.Data;
+using GestionCommande.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionCommande.Controllers
+{
+    [Route("api/commands/")]     // eq to api/commands
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        // Get api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
